43c38b68ebb295afffee04e40c3736af656634a2
jKey Lu <jkeylu@gmail.com>
2016-12-27 21:05:47 +0800

    To prevent memory leaks

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
+'use strict';
+
+const Promise = require('any-promise');
+
 function loop(fn, times) {
     if (typeof fn != 'function') {
-        throw new TypeError('fn must be a function!');
+        return Promise.reject(new TypeError('Loop fn must be a function!'));
     }
 
     let i = 0;
-    let next = function () {
+    let next = function (done) {
         if (times > 0) {
             if (++i > times) {
+                done();
                 return;
             }
         }
 
         try {
-            return Promise.resolve(fn()).then(next);
+            Promise.resolve(fn()).then(() => next(done), done);
         } catch (err) {
-            return Promise.reject(err);
+            done(err);
         }
-    }
+    };
 
-    return next();
+    return new Promise((resolve, reject) => {
+        next((err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+    });
 }
 
 module.exports = loop;
